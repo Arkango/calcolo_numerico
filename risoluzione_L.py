92b725cda1892d22615056dbf999ab053398168d
arkango <asaracino@aryma.it>
2018-12-08 12:27:37 +0100

    risoluzione esercizo matrice l funzionante

--- risoluzione_L.py
+++ risoluzione_L.py
@@ -3,16 +3,12 @@ import numpy as np
 def risolviL(L,b):
     result = []
 
-    for i in range(0,3):
-        l_current = 0
-        for x in range(0,3):
-            if x != i:
-                if i == 0 :
-                    value = 0
-                else:
-                    value = result[i-1]
-                l_current += L.item((i,x))*-1*value
-        result.append(b.item(i)*L.item((i,i)) + l_current)
+    result.append( b.item(0)/L.item(0, 0))
+    for i in range(1,3):
+        val = 0
+        for x in range(0,i):
+            val += result[x]*L.item(i,x)
+        result.append((b.item(i) + ((-1)*val))/L.item(i,i))
     return result
 
 
